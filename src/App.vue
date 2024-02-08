6fd977d637fbc13057d6d524c00f94d6e900b3e9
Willem Verbuyst <willemverbuyst@gmail.com>
2024-02-08 05:21:31 +0100

    Scaffold components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,17 @@
-<template>My app</template>
+<script setup>
+import AddTransaction from "./components/AddTransaction.vue";
+import Balance from "./components/Balance.vue";
+import Header from "./components/Header.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+</script>
+
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,1 @@
+<template><h2>Expense Tracker</h2></template>
